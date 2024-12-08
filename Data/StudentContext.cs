3c4fd5c4fbbfe14544fa623b3196523600dd8185
Vidushi232000 <vidushi02032000@gmail.com>
2024-12-08 23:26:44 +0530

    added-a-comment

--- Data/StudentContext.cs
+++ Data/StudentContext.cs
@@ -5,7 +5,7 @@ using studentdb.Model;
 
 namespace studentdb.Data
 {
-    public class StudentContext : DbContext
+    public class StudentContext : DbContext // initializing the dbcontext class
 
     {
         public DbSet<Student> Students { get; set; }
